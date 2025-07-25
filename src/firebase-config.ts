19e34c9d66339cd028726985ec3b62601b9e86a3
Ariel <juanariel1912@gmail.com>
2025-07-25 13:45:27 -0400

    refactor: update Firebase configuration to use environment variables

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlP5nYa2qJ1vWE9E9HaS_XSnrPUfPCQbw",
-  authDomain: "text-editor-project-324a0.firebaseapp.com",
-  projectId: "text-editor-project-324a0",
-  storageBucket: "text-editor-project-324a0.firebasestorage.app",
-  messagingSenderId: "168310269335",
-  appId: "1:168310269335:web:f64c97d95119be81ab10d2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
